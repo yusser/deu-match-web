4c63cb36514df8fb349a8143c7bf7ef754a157c2
Gustavo Cardoso <gcardoso@gmail.com>
2021-10-18 06:19:02 -0300

    adição de templates

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>Header</div>
+    <div>Menu</div>
+    <nuxt />
+    <div>Footer</div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Deal DeuMatch - ' + this.$route.path,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ layouts/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <nuxt />
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      htmlAttrs: {
+        class: 'dm-login-page'
+      },
+      title: 'Deal DeuMatch - ' + this.$route.path
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
